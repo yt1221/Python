c5d1243935e088eb69f56770bd7b5a688a47f08e
ゲッツ <>
2019-07-20 22:23:43 +0900

    if文学習 肥満度を表示するように処理を変更

--- study/bmi.py
+++ study/bmi.py
@@ -1,6 +1,12 @@
-weight = input('体重を入力してください。')
-height = input('身長を入力してください。')
-weight = float(weight)
-height = float(height) / 100
+weight = float(input('体重を入力してください。'))
+height = float(input('身長を入力してください。')) / 100
 bmi = weight / (height ** 2)
 print(bmi)
+if bmi < 18.5:
+    print('低体重')
+elif 18.5 <= bmi < 25:
+    print('標準体重')
+elif 25 <= bmi < 30:
+    print('前肥満')
+elif 30 <= bmi:
+    print('肥満')
